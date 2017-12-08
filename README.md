33da4da960984f12bec12e5f0b712c63cd3e14e0
Supriya <supriyaskumar28@gmail.com>
2017-12-07 17:53:11 -0800

    Updated read me

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # LinuxProfiling
 Analyzing run-time process interactions to model a complex network for threat identification
+
+
+ * Description:
+ 
+ * This script parses the output of 'stat' command on all fds' of all
+ * processes and create a complex graph out of it giving a visual of
+ * inter-connected processes. 
+
+ * Example:
+ * root@ubuntu:/home/supriya# grep "example pipe" | grep "print it to the stdout"
+
+ * root@ubuntu:~# stat /proc/2497/fd/1
+ *   File: ‘/proc/2497/fd/1’ -> ‘pipe:[19040]’
+ *   Size: 64           Blocks: 0          IO Block: 1024   symbolic link
+ * Device: 4h/4d        Inode: 19420       Links: 1
+ * Access: (0300/l-wx------)  Uid: (    0/    root)   Gid: (    0/    root)
+ * Access: 2017-12-07 17:19:01.262765577 -0800
+ * Modify: 2017-12-07 17:19:01.262765577 -0800
+ * Change: 2017-12-07 17:19:01.262765577 -0800
+ *   Birth: -
+
+ * root@ubuntu:~# ps awuxx | grep grep
+ * root   2497  0.0  0.2  15944  2236 pts/13   S+   17:18   0:00 grep --color=auto example pipe
+ * root   2498  0.0  0.2  15944  2244 pts/13   S+   17:18   0:00 grep --color=auto print it to the stdout
+
+ * root@ubuntu:~# stat /proc/2498/fd/0
+ *   File: ‘/proc/2498/fd/0’ -> ‘pipe:[19040]’
+ *   Size: 64           Blocks: 0          IO Block: 1024   symbolic link
+ * Device: 4h/4d        Inode: 19427       Links: 1
+ * Access: (0500/lr-x------)  Uid: (    0/    root)   Gid: (    0/    root)
+ * Access: 2017-12-07 17:19:07.966115817 -0800
+ * Modify: 2017-12-07 17:19:07.966115817 -0800
+ * Change: 2017-12-07 17:19:07.966115817 -0800
+ *   Birth: -
+
+ * The script captures ‘pipe:[19040]’ and connects all processes (2497 and 2498)  using this.
+
+ * The output of first part - the parser is a dictionary mapping IPCs to its corresponding processes.
+ * For the above example, the output looks like -
+ * {'pipe:[19040]’: ['2497', '2498']} 
+
+ * This dictionary is then passed to a graph generator, which generates the complex network graph of
+ * all IPCs depicting their inter-connection.
--- stat_parser.py
+++ stat_parser.py
@@ -1,19 +1,37 @@
-import subprocess
-import re
-import networkx as nx
+"""
+ * file:  stat_parser.py 
+ * date:  Wed Dec 06 2017 
+ *
+ * Description:
+ * This script parses the output of 'stat' command on all fds' of all
+ * processes and create a complex graph out of it giving a visual of
+ * inter-connected processes.
+"""
 
+
+# Import module for graph functions
+import itertools
 import matplotlib
 matplotlib.use('Agg')
-
 import matplotlib.pyplot as plt
-import itertools
+import networkx as nx
+
+# Import modules for parsing
+import re
+import subprocess
 from collections import defaultdict
 
+print ("***> Executing 'stat' command...")
+print ("It might take a while!") 
+
+# Execute the 'stat' command on all fds' pf all processes
 process = subprocess.Popen('stat /proc/*/fd/*', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 out, err = process.communicate()
 new = re.sub(u"(\u2018|\u2019)", "'", out.decode('utf-8'))
 
-print ("Filename matching:")
+print ("***> Parsing the output of the command...")
+
+# Parse the output to get the process id and its IPC
 fnode = re.compile(r"File:\s+'/proc/(\d*)/fd/\d*'\s+->\s+'(.*)'.*")
 fnode_list = fnode.findall(new)
 fnode_list = [(x.encode("utf-8"), y.encode("utf-8")) for (x, y) in fnode_list]
@@ -21,36 +39,41 @@ fnode_list = [(x.encode("utf-8"), y.encode("utf-8")) for (x, y) in fnode_list]
 fnode_dict = defaultdict(list)
 for k, v in fnode_list:
   fnode_dict[v].append(k)
+
+print ("***> Creating a dictionary mapping pid and its IPC")
+
+# Remove slef sockets/pipes, '/dev/*/' communications as well as 'anone_inode' events 
 new_dict = {} 
 for i in fnode_dict:
   if len(set(fnode_dict[i])) > 1:
     if (not "/dev/" in i and not "anon_inode:[eventfd]" in i): 
       new_dict[i] = fnode_dict[i]
-      print i, set(fnode_dict[i])
-
-print ("_______________________________________________________________________________________________________________________________________________________________________________")
+      #print i, set(fnode_dict[i])
 
 
+# Polish the dict to generate the graph
 nodes = []
 for i in new_dict:
   nodes = list(set(nodes) | set(new_dict[i]))
 
+print ("***> Generating the graph...")
+print ("It might take a while!") 
+
+# Generate the graph and save it as 'complex.png'
 count = 1
-print ("length: ", len(new_dict))
 for i in new_dict:
   cmd = 'G'+str(count)
-  print cmd
   cmd=nx.Graph()
   nodes = list(set(new_dict[i]))
   cmd.add_nodes_from(nodes)
   edges = list(itertools.combinations((set(new_dict[i])), r=2))
-  print ("_______________________________________________________________________________________________________________________________________________________________________________")
   cmd.add_edges_from(edges)
   cmd.graph['graph']={'label':i,'labelloc':'t'}
   nx.draw(cmd, node_size=5, node_color = 'green', with_labels = True)
-  #plt.savefig('Graph'+str(count)+'.png')
-  #plt.show()
   count += 1
   if count == (len(new_dict.keys()))+1:
     plt.savefig('complex.png')
     break
+
+print ("***> Generated the complex network graph of IPCs!")
+print ("***> Graph has been saved as 'complex.png'")
